f7caad8f90be73e5bb3e894d6e2caf62ae181f5a
Art Eschenlauer <eschen@alumni.princeton.edu>
2019-02-19 10:52:53 -0600

    cleanup scripts and strategy (particularly signal handling and container lifecycle)

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
-# Compose Docker using `docker-galaxy-stable` image
+# Compose Galaxy using `bgruening/galaxy-stable`
 
-## Part 1 - creating and initializing a data-storage container
-### create storage container named galaxy-store
+## Part 1 - create and initialize a data-storage container
+
+### Create storage volume galaxy-store
 ```bash
 docker volume create galaxy-store
 docker volume inspect galaxy-store
+```
+### Create storage container named galaxy-store
+```bash
 docker create --mount source=galaxy-store,target=/export --name galaxy-store bgruening/galaxy-stable /bin/true
 ```
-### init "galaxy" using galaxy-store as export (so export files get copied to volume)
+### Initialize the storage volume (so export files get copied to volume)
+Run the fullowing till it starts the web server:
 ```bash
 docker run --rm -ti -p 8080:80 --volumes-from galaxy-store --name galaxy_bootstrap bgruening/galaxy-stable
 ```
-Then use ^C to quit after export volume is initialized
+then use ^C to quit after export volume is initialized
 
-## Part 2 - run and enjoy
+## Part 2 - Run the composed Galaxy
 
-After making sure that `.env` is up to date ...
+Copy default.env.example to default.env (or .env on Windows).
+Make sure that `.env` is up to date, then run:
 
 ```bash
 ./start.sh
--- start.sh
+++ start.sh
@@ -1,40 +1,60 @@
 #!/bin/bash
 
-# get directory for script
+# get directory containinng this script
 # ref: https://stackoverflow.com/a/246128
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
   DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  # if $SOURCE was a relative symlink, we need to resolve it 
+  #    relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
 done
 export DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+SCRIPT=$(basename ${SOURCE})
 
+# routione report caught signals
 _term() { 
-  echo "Caught signal $1 in $2" 
+  echo "${SCRIPT}: Caught signal $1 in $2" 
   cd $DIR
-  docker-compose exec galaxy-postgres supervisorctl stop postgresql
-  # kill -INT $CHILD 
 }
 
+# trap signals to invoke _term when they are raised
+#   rather than aborting the whole script.
+trap "{ echo ${SCRIPT}: INT  $DIR; _term INT  $DIR; }" INT
+trap "{ echo ${SCRIPT}: QUIT $DIR; _term QUIT $DIR; }" QUIT
+trap "{ echo ${SCRIPT}: ABRT $DIR; _term ABRT $DIR; }" ABRT
+trap "{ echo ${SCRIPT}: TERM $DIR; _term TERM $DIR; }" TERM
+
+# execute from the directory containing this script
 pushd $DIR
 
-trap "{ echo INT  $DIR; _term INT  $DIR; }" INT
-trap "{ echo QUIT $DIR; _term QUIT $DIR; }" QUIT
-trap "{ echo ABRT $DIR; _term ABRT $DIR; }" ABRT
-trap "{ echo TERM $DIR; _term TERM $DIR; }" TERM
-trap "{ echo EXIT $DIR; exit 0; }" EXIT
-
+# compose Galaxy from services defined in the YAML file
 docker-compose -f docker-compose.yml up --remove-orphans &
-sleep 5
-docker-compose ps
-
 export CHILD=$! 
-echo child PID is $CHILD now waiting for same
+
+# wait 5 seconds, then show status of docker-compose containers
+sleep 5
+echo "${SCRIPT}: Here are the running containers."
+docker-compose -f docker-compose.yml ps
+
+# wait for composition to exit or this script to be interrupted
+echo "${SCRIPT}: Child PID is $CHILD."
+echo "${SCRIPT}: Now waiting for process to exit or this script to be interrupted."
 wait "$CHILD"
 
-echo child is gone
+echo ${SCRIPT}: Child is gone
+# shutdown postgresql gracefully
+docker-compose exec galaxy-postgres supervisorctl stop postgresql
+# shutdown the supervisor and remaining processes gracefully
+docker-compose exec galaxy-postgres supervisorctl shutdown
+docker-compose exec galaxy-stable   supervisorctl shutdown
+# shut down all services in the composition
 docker-compose -f docker-compose.yml down --volumes
-echo farewell
+echo "${SCRIPT}: Here are the running containers."
+docker-compose -f docker-compose.yml ps
+echo ${SCRIPT}: Farewell
 
+# restore the working directory (only applicable when script is sourced)
 popd
